7d15183ee60e62f2aeb15bc883b5b0a1a7496aac
Thang Pham <phamtoanthang@gmail.com>
2016-03-14 00:27:26 +0700

    Instruction folloed

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -2,7 +2,17 @@ class FoodItem < ActiveRecord::Base
   def self.filter_by_section(section)
   	where(section: section)
   end
+  
   def self.sort_by_name(name)
   	order(name: :asc)
   end
+
+  def url_or_auto
+  	if url.present?
+  		url
+  	else
+  		"http://loremflickr.com/320/240/#{name}"
+  	end
+  end
+
 end
